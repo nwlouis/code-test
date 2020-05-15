252d6221ead71e8d05851d94ad4ddf9443d585d3
nwlouis <64062613+nwlouis@users.noreply.github.com>
2020-05-15 22:04:46 +0800

    Add files via upload

--- file_manage/views.py
+++ file_manage/views.py
@@ -8,13 +8,17 @@ from collections import OrderedDict
 from django.views.generic.edit import FormMixin
 from django.urls import reverse_lazy, reverse
 from django.views.generic.list import ListView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
+@login_required
 def index(request):
     customer_list = Customer.objects.order_by('ref')
     cuslist = {'customers':customer_list}
     return render(request, 'file_manage/index.html', context = cuslist)
 
+@login_required
 def new_customer(request):
     customerForm = CustomerForm()
     if request.method == "POST":
@@ -26,16 +30,19 @@ def new_customer(request):
             print('invalid form')
     return render(request, 'file_manage/new_customer.html',{'customerForm':customerForm})
 
-class customerInfoview(DetailView):
+
+class customerInfoview(LoginRequiredMixin, DetailView):
     model = Customer
     template_name = "file_manage/customer_info.html"
     def get_success_url(self):
         return reverse('customer_info', kwargs={'pk': self.object.pk})
 
-class customerEditview(UpdateView):
+
+class customerEditview(LoginRequiredMixin, UpdateView):
     model = Customer
     form_class = CustomerForm
 
-class customerRemoveview(DeleteView):
+
+class customerRemoveview(LoginRequiredMixin, DeleteView):
     model = Customer
     success_url = reverse_lazy('index')
